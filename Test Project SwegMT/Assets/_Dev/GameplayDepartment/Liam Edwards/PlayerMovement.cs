13211e382a45ea72f30e34bfc4f5f6f45117ffa4
xxliam32xx <liamedwardsellis@hotmail.com>
2023-10-06 12:13:53 +0100

    Player movement test
    Added simple walk and run feature

---
+++ Test Project SwegMT/Assets/_Dev/GameplayDepartment/Liam Edwards/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declare Variables 
+    public float walkSpeed = 6f; // Variable to store the walking speed
+    public float sprintSpeed = 8f; // Variable to store the srint speed
+
+    //Update Method  
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+
+
+    public void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * walkSpeed; // Horizontal input multiplied with Time.deltaTime and walkSpeed to get the horizontal movement.
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * walkSpeed; // Vertical input multiplied with Time.deltaTime and walkSpeed to get the horizontal movement.
+
+
+        if (Input.GetKey(KeyCode.LeftShift)) // Multiplies the horizontal and vertical variables by a sprintSpeed value. This will increase the speed of the character, simulating a sprint.
+        {
+            horizontal *= sprintSpeed;
+            vertical *= sprintSpeed;
+        }
+
+        transform.Translate(horizontal, 0f, vertical); // Move the object along the x-axis by 'horizontal' units and along the z-axis by 'vertical' units, leaving the y-axis unchanged.
+    }
+}
+
+
