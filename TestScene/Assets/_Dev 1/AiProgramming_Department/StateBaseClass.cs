eee5a274d54d3eb950fcaa25a405bd75f8c80bf4
TheMGLegends <120389432+TheMGLegends@users.noreply.github.com>
2023-10-10 14:09:02 +0100

    Full Project Merge READ DESC - Somi xoxo
    **FROM NOW ON PLEASE OPEN TESTSCENE AS A UNITY PROJECT**
    
    NOT testprojectswegMT

---
+++ TestScene/Assets/_Dev 1/AiProgramming_Department/StateBaseClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateBaseClass
+{
+    public virtual void UpdateLogic()
+    {
+        //handles the logic for each individual state
+        //need to liaise with leads
+    }
+}
