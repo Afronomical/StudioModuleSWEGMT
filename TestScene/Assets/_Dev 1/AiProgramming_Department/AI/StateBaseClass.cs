9770f04e13e17a316c7e82141af8eeb988543e9e
luciandusciac <DUSCIACLUCIAN@YAHOO.COM>
2023-10-11 13:59:10 +0100

    Changed npc states from 3d to 2d functionality and tested

--- TestScene/Assets/_Dev 1/AiProgramming_Department/AI/StateBaseClass.cs
+++ TestScene/Assets/_Dev 1/AiProgramming_Department/AI/StateBaseClass.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateBaseClass
+public class StateBaseClass : MonoBehaviour
 {
+    public AICharacter character;
+
     public virtual void UpdateLogic()
     {
         //handles the logic for each individual state
