32200cba642baff42826773ff55eac6f00f5b821
xxliam32xx <liamedwardsellis@hotmail.com>
2023-10-20 12:40:27 +0100

    re made dodge mechanic
    - the player moves more smoothly across the screen now

--- TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/PlayerMovement.cs
+++ TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/PlayerMovement.cs
@@ -7,37 +7,57 @@ public class PlayerController : MonoBehaviour
     //Declare Variables 
     public float speed;
     public float sprintSpeed;
-    public float jumpSpeed;
     private Rigidbody2D rb;
-
-
+    [SerializeField] float dodgeSpeed = 10f;
+    [SerializeField] float dodgeDuration = 1f;
+    [SerializeField] float dodgeCooldown;
+    bool isDodging;
+    bool canDodge;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canDodge = true;
     }
 
     void Update()
     {
 
-        PlayerMovement();
+        if (isDodging)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * sprintSpeed, Input.GetAxis("Vertical") * sprintSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && canDodge)
+        {
+            StartCoroutine(Dodge());
+        }
+        else 
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        }
 
     }
 
 
 
-    public void PlayerMovement()
+
+    private IEnumerator Dodge()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * sprintSpeed, Input.GetAxis("Vertical") * sprintSpeed);
-        }
-        else
-        {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
-        }
 
+        canDodge = false;
+        isDodging = true;
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * dodgeSpeed, Input.GetAxis("Vertical") * dodgeSpeed);
+        yield return new WaitForSeconds(dodgeDuration);
+        isDodging = false;
 
+        yield return new WaitForSeconds(dodgeCooldown);
+        canDodge = true;
+        
     }
 
 }
