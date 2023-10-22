4c26a2be110b4fc84cbae67063bf10fb0c447900
Alex Izak <izak_alex@yahoo.com>
2023-10-22 19:01:55 +0100

    Attempted to create validation
    We tried to make the algorithm smarter and the generated map look more natural.

--- TestScene/Assets/_Dev 1/AiProgramming_Department/AI/ProceduralGeneration.cs
+++ TestScene/Assets/_Dev 1/AiProgramming_Department/AI/ProceduralGeneration.cs
@@ -4,12 +4,39 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public class Tile
+{
+    Tile(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    Tile(int x, int y, TileBase sprite) 
+    {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+    }
+    public int x, y;
+    public TileBase sprite;
+    public Tile upNeighbour;
+    public Tile downNeighbour;
+    public Tile leftNeighbour;
+    public Tile rightNeighbour;
+
+    //public Tile GetAdjacentTile(Tile origin, int x, int y)
+    //{
+    //    Tile tileToReturn = Tile(origin.x + x, origin.y + y);
+
+    //    return tileToReturn;
+    //}
+}
+
 public class ProceduralGeneration : MonoBehaviour 
 {
     int[,] map;
     public TileBase ground_Tile;
     public Tilemap tiles;
-
 
     public TileBase tree_Tile;
     public TileBase house_Tile;
@@ -17,6 +44,8 @@ public class ProceduralGeneration : MonoBehaviour
 
     public int numberOfHouses;
     public int numberOfFences;
+
+    List<Tile> tileLocations;
 
     //Creating the Grid
     public int[,] GenerateArray(int width, int height)
@@ -54,11 +83,6 @@ public class ProceduralGeneration : MonoBehaviour
         {
             return 4;
         }
-        //else
-        //{
-        //    return tile; //Collision tile
-        //}
-        //return 0;
     }
 
     //Displaying art on the grid
@@ -78,31 +102,28 @@ public class ProceduralGeneration : MonoBehaviour
                 //Add more checks (with a switch) depending on how many different tiles we want to spawn
                 switch(map[x, y])
                 {
-                    //case 1:
-                    //    tilemap.SetTile(new Vector3Int(x, y, 0), house_Tile);
-
-                    //    break;
-
                     case 2:
+                        //Tile treeTile = new Tile(x, y, tree_Tile);
+                        //treeTile.x = x;
+                        //treeTile.y = y;
+                        //treeTile.sprite = tree_Tile;
                         tilemap.SetTile(new Vector3Int(x, y, 0), tree_Tile);
+                        //tileLocations.Add(treeTile);
                         break;
 
                     case 3:
+                        //Tile groundTile = new Tile();
+                        //groundTile.x = x;
+                        //groundTile.y = y;
+                        //groundTile.sprite = tree_Tile;
                         tilemap.SetTile(new Vector3Int(x, y, 0), ground_Tile);
+                        //tileLocations.Add(groundTile);
                         break;
-
-                    //case 4:
-                    //    tilemap.SetTile(new Vector3Int(x, y, 0), fence_Tile);
-                    //    break;
 
                     default:
 
                         break;
                 }
-                //if (map[x, y] == 1)
-                //{
-                //    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-                //}
             }
         }
     }
@@ -114,40 +135,57 @@ public class ProceduralGeneration : MonoBehaviour
         tilemap.AddComponent<TilemapCollider2D>();
         //tilemap.AddTileFlags()
         //Loop through the width of the map
+
+        int housesOnRow = 0;
+
         for (int x = 0; x < map.GetUpperBound(0); x++)
         {
+            ++housesOnRow;
+
             //Loop through the height of the map
             for (int y = 0; y < map.GetUpperBound(1); y++)
             {
+
                 // 1 = tile, 0 = no tile 
                 //Add more checks (with a switch) depending on how many different tiles we want to spawn
                 switch (map[x, y])
                 {
                     case 1:
-                        tilemap.SetTile(new Vector3Int(x, y, 0), house_Tile);
+                        if(housesOnRow < 3)
+                        {
+                            //Tile houseTile = new Tile();
+                            //houseTile.x = x;
+                            //houseTile.y = y;
+                            //houseTile.sprite = tree_Tile;
+                            //houseTile.upNeighbour = 
+                            tilemap.SetTile(new Vector3Int(x, y, 0), house_Tile);
+                            //tileLocations.Add(houseTile);
+                        }
+                        else
+                        {
+                            break;
+                        }
 
                         break;
 
-                    //case 2:
-                    //    tilemap.SetTile(new Vector3Int(x, y, 0), tree_Tile);
-                    //    break;
-
-                    //case 3:
-                    //    tilemap.SetTile(new Vector3Int(x, y, 0), ground_Tile);
-                    //    break;
-
                     case 4:
+                        //Tile fenceTile = new Tile();
+                        //fenceTile.x = x;
+                        //fenceTile.y = y;
+                        //fenceTile.sprite = tree_Tile;
+                        //fenceTile.upNeighbour = new Vector2(x, y + 1);
+                        //fenceTile.downNeighbour = new Vector2(x, y - 1);
+                        //fenceTile.leftNeighbour = new Vector2(x + 1, y);
+                        //fenceTile.rightNeighbour = new Vector2(x - 1, y);
                         tilemap.SetTile(new Vector3Int(x, y, 0), fence_Tile);
+                        //tileLocations.Add(fenceTile);
                         break;
 
                     default:
 
                         break;
                 }
-                //if (map[x, y] == 1)
-                //{
-                //    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-                //}
+                housesOnRow = 0;
             }
         }
     }
@@ -173,6 +211,14 @@ public class ProceduralGeneration : MonoBehaviour
 
                 //Add different logic based on what we need to do
             }
+        }
+    }
+
+    public void ValidateMap()
+    {
+        foreach (Tile item in tileLocations)
+        {
+            //if(item.upNeighbour == )
         }
     }
 }
