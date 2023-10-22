9017c7dadfdb29a7286166eac553d4bb7aedf8af
ILikeTurtles <134623253+UsernameNotTakenXD@users.noreply.github.com>
2023-10-22 16:25:35 +0100

    Tidying code/Commenting
    - Made the code more readable and added/edited comments

--- TestScene/Assets/_Dev 1/AiProgramming_Department/AI/AttackState.cs
+++ TestScene/Assets/_Dev 1/AiProgramming_Department/AI/AttackState.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Pair programmed: Adam (Driver) and Aaron (Navigator)
 public class AttackState : StateBaseClass
 {
     public int attackDamage = 10;
@@ -9,54 +11,34 @@ public class AttackState : StateBaseClass
     private float currentDelay;
     
     //Gameplay Programmers Script for the Player Health
-    private PlayerDeath playerDeath;  
-    public ReferenceManager refMan;
-
+    private PlayerDeath playerDeath;
     public AttackState() 
     {
+        //When a character goes to the attack state, this will delay the attack by x amount
         currentDelay = 0.2f;
     }
     
     public override void UpdateLogic()
     {
-        
         //Set the reference for the playerDeath variable
         if (playerDeath == null)
         {
             playerDeath = character.player.GetComponent<PlayerDeath>();
         }
         
-        Debug.Log("Is attacking");
+        Debug.Log("AI is attacking");
 
-
+        //Counts down the delay
         currentDelay -= Time.deltaTime;
 
-
-        if(currentDelay <= 0)
+        //Checks if the delay timer has hit 0, if so, it will damage the player and reset the delay timer to x amount
+        if (currentDelay <= 0)
         {
             playerDeath.SetHealth(attackDamage);
             currentDelay = 2;
         }
-
-
-
-
-        //Checks if the delay timer has hit 0, if so, it will damage the player and reset the delay timer to x amount
-        //if (refMan.aiattackDelay <= 0)
-        //{
-        //    refMan.StartCount = false;
-        //    playerDeath.SetHealth(attackDamage);
-        //    refMan.aiattackDelay = attackDelay;
-        //    refMan.StartCount = true;
-        //}
-
+        
         //this was moved to HuntState
         //character.transform.position = Vector2.MoveTowards(character.transform.position, character.player.transform.position, speed * Time.deltaTime / 2);
-
-
     }
-
-    
-
-
 }
