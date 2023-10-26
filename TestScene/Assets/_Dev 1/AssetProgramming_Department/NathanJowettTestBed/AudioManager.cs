247ba2a80b1084b99fbce3d18bb38b07386c0d61
Nathan <nat0tpsn@gmail.com>
2023-10-26 18:05:44 +0100

    Updated audio manager
    Reworked AudioManager, created a vfx and a music controller so that we can add any amount of effects/sounds from the inspector, also added ability for each element to have its own volume, pitch and looping setting.

--- TestScene/Assets/_Dev 1/AssetProgramming_Department/NathanJowettTestBed/AudioManager.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/NathanJowettTestBed/AudioManager.cs
@@ -1,5 +1,189 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public VFX_Controller[] Sounds;
+    public Music_Controller[] Music;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (VFX_Controller s in Sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.Loop;
+        }
+
+        foreach (Music_Controller m in Music)
+        {
+            m.source = gameObject.AddComponent<AudioSource>();
+            m.source.clip = m.clip;
+            m.source.volume = m.volume;
+            m.source.pitch = m.pitch;
+            m.source.loop = m.Loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        VFX_Controller s = Array.Find(Sounds, VFX_Controller => VFX_Controller.Name == name);
+
+        Music_Controller m = Array.Find(Music, Music_Controller => Music_Controller.Name == name);
+
+
+        if (s == null || m == null)
+        {
+            Debug.Log("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+        m.source.Play();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//Old Manager
+/*public class AudioManager : MonoBehaviour
+{
+    public enum SoundEffects
+    {
+        //Player Effects
+        PlayerTakeDamage,
+        PlayerDeath,
+        PlayerAttack,
+        PlayerFeed,
+        PlayerDodge,
+        PlayerHeal,
+        PlayerMoving,
+
+        //NPC Sounds
+        NPC_TakeDamage,
+        NPC_Death,
+        NPC_Downed,
+        NPC_AttackMelee,
+        NPC_AttackRanged,
+        NPC_Scream,
+        NPC_Alterted,
+        NPC_VilToHunter,
+
+        //UI SFX
+        ButtonClick,
+        *//*UpgradeAchieved,
+
+        //*
+    }
+
+    // INFO: The order of sound effects and audio clips should be exactly the same, otherwise the incorrect sound will be played
+    [SerializeField] private List<SoundEffects> soundEffectsList = new();
+    [SerializeField] private List<AudioClip> audioClipsList = new();
+    [SerializeField] private Dictionary<SoundEffects, AudioClip> soundEffectsDictionary = new();
+
+    [SerializeField] private GameObject soundEffectsPlayerPrefab;
+
+    private float soundVolume = 1f;
+
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < soundEffectsList.Count; i++)
+        {
+            soundEffectsDictionary.Add(soundEffectsList[i], audioClipsList[i]);
+        }
+    }
+
+    public void PlaySoundEffect(SoundEffects soundEffect)
+    {
+        if (soundEffectsDictionary.ContainsKey(soundEffect))
+        {
+            GameObject GO = Instantiate(soundEffectsPlayerPrefab);
+            GO.GetComponent<AudioSource>().PlayOneShot(soundEffectsDictionary[soundEffect], soundVolume);
+            Destroy(GO, soundEffectsDictionary[soundEffect].length);
+        }
+        else
+        {
+            Debug.Log("Sound not found!");
+        }
+    }
+
+    public void SoundEffectVolume(float volume)
+    {
+        // INFO: Changed via slider for sound effect volume bar inside of settings menu
+        soundVolume = volume;
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -27,7 +211,7 @@ public class AudioManager : MonoBehaviour
 
         //UI SFX
         ButtonClick,
-        /*UpgradeAchieved,
+        *//*UpgradeAchieved,
 
         //Background Music Types
         MenuMusic,
@@ -35,7 +219,7 @@ public class AudioManager : MonoBehaviour
         PlayerHuntedMusic,
         BossMusic,
         DeathEndMusic,
-        BossVictoryMusic*/
+        BossVictoryMusic*//*
     }
 
     // INFO: The order of sound effects and audio clips should be exactly the same, otherwise the incorrect sound will be played
@@ -90,3 +274,4 @@ public class AudioManager : MonoBehaviour
         soundVolume = volume;
     }
 }
+*/
---
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/NathanJowettTestBed/Music_Controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Music_Controller
+{
+
+    public string Name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)] public float volume;
+    [Range(.1f, 3f)] public float pitch;
+
+    public bool Loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
