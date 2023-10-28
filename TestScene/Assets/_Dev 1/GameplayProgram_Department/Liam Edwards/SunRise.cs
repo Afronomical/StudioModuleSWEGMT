2db6f7bf46c48364f8d09af4a097b5b7453abbca
xxliam32xx <liamedwardsellis@hotmail.com>
2023-10-28 11:41:03 +0100

    Wall of Sun Light

--- TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/SunRise.cs
+++ TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/SunRise.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class SunRise : MonoBehaviour
 {
    
-    public float maxScale;
-    public float minScale;
-    public float scaleSpeed; // How Fast The Sun Light Moves
+    public float scaleSpeed = 3; // How Fast The Sun Light Moves
     private bool canBurn = false;
-    private float damageDelay = 1; // The delay Btween Damage While In The Sun Light
-    public float nightTime; 
+    private float damageDelay = 1; // The delay Between Damage While In The Sun Light
+    public float nightTime; // The amount of time remaining in the night
     public CountdownTimer countdownTimer;
 
 
     public void Start()
-    {   
+    {
+
+        gameObject.transform.localScale = new Vector2(0.1f, 100); // Sets Size Of SunLight Game Object (width, height)
         transform.position = new Vector3(52, 0, 0.5f); // Sets Pos to Off The Screen
         canBurn = true;
-        gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<Renderer>().enabled = false; // Disables Sun Light
+
     }
+
 
 
 
@@ -35,22 +37,13 @@ public class SunRise : MonoBehaviour
         {
             
             gameObject.GetComponent<Renderer>().enabled = true; // When Countdown Reaches 0 SunLight Object Will Activate
+            transform.localScale += new Vector3(scaleSpeed, scaleSpeed, 0) * Time.deltaTime; // gradually increase the local scale of the object over time.
 
-            if (transform.localScale.x <= maxScale && transform.localScale.y <= maxScale)
-            {
-               transform.localScale += new Vector3(scaleSpeed, scaleSpeed, 0) * Time.deltaTime;
-            }
-            else
-            {
-               transform.localScale = new Vector3(minScale, minScale, 0);
-            }
             if (transform.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>()) && canBurn == true) // When Player Touches Sun Light And canBurn = true 
             {   
-                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().SunRiseDamage(); 
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().SunRiseDamage(); // Calls The Function "SunRiseDamage" From "playerDeath" Script
                 StartCoroutine(sunDamage()); 
             }
-
-
 
         }
 
