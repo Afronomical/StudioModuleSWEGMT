d1127eb5c9a168df99c03c94847a98471e073044
luciandusciac <DUSCIACLUCIAN@YAHOO.COM>
2023-11-01 18:35:26 +0000

    Implemented 360 shooter

--- TestScene/Assets/_Dev 1/AiProgramming_Department/AI/Scripts/ShootState.cs
+++ TestScene/Assets/_Dev 1/AiProgramming_Department/AI/Scripts/ShootState.cs
@@ -12,7 +12,7 @@ public class ShootState : StateBaseClass
 
     public Transform origin;
     public GameObject bulletPrefab;
-    private float bulletSpeed = 500f;
+    private float bulletSpeed = 300f;
     
 
     //Gameplay Programmers Script for the Player Health
@@ -28,7 +28,7 @@ public class ShootState : StateBaseClass
     public ShootState()
     {
         
-        currentDelay = 0.8f;
+        currentDelay = 2f;
     }
 
     public override void UpdateLogic()
@@ -37,23 +37,52 @@ public class ShootState : StateBaseClass
         this.GetComponent<SpriteRenderer>().color = Color.cyan;
         character.isMoving = false;
 
-        Vector3 vectorToTarget = Quaternion.Euler(0, 0, 90) * (character.player.transform.position - transform.position);  // Direction towards the target location
-        transform.rotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: vectorToTarget);
+
 
         //Counts down the delay
-        currentDelay -= Time.deltaTime;
+        //currentDelay -= Time.deltaTime;
 
-        if (currentDelay <= 0)
-        {
-            Shoot();
-            currentDelay = attackDelay;
-        }
+        //if (currentDelay <= 0)
+        //{
+        
+            CircularShoot();
+        //    currentDelay = attackDelay;
+        //}
     }
 
     void Shoot()
     {
+        Vector3 vectorToTarget = Quaternion.Euler(0, 0, 90) * (character.player.transform.position - transform.position);  // Direction towards the target location
+        transform.rotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: vectorToTarget);
+
         Vector2 distance = character.player.transform.position - character.transform.position;
         GameObject bullet = Instantiate(bulletPrefab, origin.position, origin.rotation);
         bullet.GetComponent<Rigidbody2D>().velocity = distance * bulletSpeed * Time.deltaTime;
+    }
+
+
+    void SprayShoot()
+    {
+
+        
+    }
+
+    void CircularShoot()
+    {
+        transform.Rotate(Vector3.forward * 300f * Time.deltaTime);
+        origin = character.transform;
+        GameObject bullet = Instantiate(bulletPrefab, origin.position, origin.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed * Time.deltaTime;
+    }
+
+    void ShootHomingBullet()
+    {
+
+    }
+
+    void InstantiateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, origin.position, origin.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed * Time.deltaTime;
     }
 }
