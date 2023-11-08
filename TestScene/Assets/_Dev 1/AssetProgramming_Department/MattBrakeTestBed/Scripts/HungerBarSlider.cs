9dada8f3d42780fc408e144af0d8d4f412ffad7d
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-08 18:06:55 +0000

    changes to hunger bar

--- TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/HungerBarSlider.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/HungerBarSlider.cs
@@ -7,18 +7,24 @@ public class HungerBar : MonoBehaviour
 {
     public Slider slider;
     public BlinkEffect BlinkEffect;
+    private float targetValue;
+    private float currentValue;
+    public float fillSpeed = 1f;
     
 
 
     public void SetMinHunger(int Hunger)
     {
         slider.minValue = Hunger;
-        slider.value = Hunger;   
+        slider.value = Hunger; 
+        currentValue = Hunger;
+        targetValue = currentValue;
+       
     }
 
     public void SetHunger(int Hunger)
     {
-        slider.value = Hunger;
+        targetValue = Hunger;
 
         if(slider.value >= slider.maxValue)
         {
@@ -27,6 +33,15 @@ public class HungerBar : MonoBehaviour
         else
         {
             BlinkEffect.enabled = true; 
+        }
+    }
+
+    public void Update()
+    {
+        if(currentValue != targetValue)
+        {
+            currentValue = Mathf.Lerp(currentValue, targetValue, fillSpeed * Time.deltaTime);
+            slider.value = currentValue;
         }
     }
 
