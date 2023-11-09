8a6b2925b5c25bdf49315aa96e1102d6ef947ca4
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-09 22:31:24 +0000

    continued with death screen

--- TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/DeathScreen.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/DeathScreen.cs
@@ -7,16 +7,53 @@ using UnityEngine.UI;
 public class DeathScreen : MonoBehaviour
 {
     public CanvasGroup deathScreenCanvasGroup;
+    [SerializeField] private bool fadeIn = false;
+    [SerializeField] private bool fadeOut = false;
     public float fadeInSpeed = 2f;
-    
-  public void ShowUI()
+
+    private void Start()
     {
-        deathScreenCanvasGroup.alpha = 1;
+        deathScreenCanvasGroup.alpha = 0; 
+    }
+
+
+
+    public void ShowUI()
+    {
+       // deathScreenCanvasGroup.alpha = 1;
+        fadeIn = true;
     }
 
     public void HideUI()
     {
-        deathScreenCanvasGroup.alpha = 0;
+       // deathScreenCanvasGroup.alpha = 0;
+        fadeOut = true; 
+    }
+
+    private void Update()
+    {
+        if(fadeIn)
+        {
+            if(deathScreenCanvasGroup.alpha < 1)
+            {
+                deathScreenCanvasGroup.alpha += Time.deltaTime;
+                if(deathScreenCanvasGroup.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+        if (fadeOut)
+        {
+            if (deathScreenCanvasGroup.alpha > 0)
+            {
+                deathScreenCanvasGroup.alpha -= Time.deltaTime;
+                if (deathScreenCanvasGroup.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
     }
 
 }
