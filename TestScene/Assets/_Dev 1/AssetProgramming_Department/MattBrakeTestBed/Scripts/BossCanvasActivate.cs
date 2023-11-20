f95a9b44189edb96e5ed2657b5976c4b9329a42f
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-20 14:17:08 +0000

    canvas loads up in boss scene

---
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/BossCanvasActivate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCanvasActivate : MonoBehaviour
+{
+    public Canvas BossCanvas;
+    public CanvasGroup bossCanvasGroup;
+    public bool fadeIn; 
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossCanvasGroup.alpha = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null)
+        {
+            if(collision.CompareTag("Player"))
+            {
+                fadeIn= true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeIn)
+        {
+            if (bossCanvasGroup.alpha < 1)
+            {
+               bossCanvasGroup.alpha += Time.deltaTime;
+                if (bossCanvasGroup.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+    }
+}
