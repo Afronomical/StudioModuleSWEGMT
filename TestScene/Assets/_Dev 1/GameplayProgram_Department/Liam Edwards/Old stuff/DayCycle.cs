8af0fc874299f5e448333e73977d97e4ae691264
xxliam32xx <liamedwardsellis@hotmail.com>
2023-12-03 19:32:40 +0000

    Sun Damage fix

--- TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/Old stuff/DayCycle.cs
+++ TestScene/Assets/_Dev 1/GameplayProgram_Department/Liam Edwards/Old stuff/DayCycle.cs
@@ -1,90 +1,86 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DayCycle : MonoBehaviour
 {
     public Gradient colorGradient; // A gradient to hold the color
-
-    public float changeSpeed; // The speed at which the colour changes
-    //private float colourChange = 0f; // A float to hold the current colour
+    public float changeSpeed; // The speed at which the color changes
     public float timeInSun = 0; // A float to calculate the amount of time the player has spent in the sun
-    public float nightTime; // The amount of time remaining in the night
-    public int sunBurn; //
-    public float damageDelay = 1.5f; // Num of seconds between the delay in damage whrn the player is in the sun
+    public float nightTime = 120f; // The amount of time remaining in the night
+    public int sunBurn; // Damage inflicted by the sun
+    public float damageDelay = 1.5f; // Num of seconds between the delay in damage when the player is in the sun
 
     public bool canBurn = true; // Bool to check if the player can take damage
 
     public CountdownTimer countdownTimer;
     public PlayerDeath playerDeath;
-    
-    
 
-    public void Start()
+    private void Start()
+    {
+        InitializeDayCycle();
+    }
+
+    private void Update()
+    {
+        UpdateNightTime();
+
+        if (nightTime <= 0)
+        {
+            HandleSunExposure();
+        }
+    }
+
+    private void InitializeDayCycle()
     {
         canBurn = true;
-        gameObject.transform.localScale = new Vector2(1000, 1000); // Sets Size Of SunLight Game Object (width, height) Needs to cover the whole level
-        transform.position = new Vector3(0, 0, 0); // Sets Pos to Off The Screen
-
-        //Color currentColour = colorGradient.Evaluate(colourChange);
-        //GetComponent<Renderer>().material.color = currentColour;
-        
+        gameObject.transform.localScale = new Vector2(1000, 1000);
+        transform.position = new Vector3(0, 0, 0);
+        nightTime = 120;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    private void UpdateNightTime()
     {
-       
-        nightTime = countdownTimer.timeRemaining; // Links Up The "nightTime" Variable To The Countdown Clock
-
-        if (nightTime <= 0) // Checks For When The Timer Reaches 
-        {            
-            ColourChange();
-            timeInSun += Time.deltaTime;
-            if (transform.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>()))
-            {                              
-                if (timeInSun < 5 && canBurn == true)
-                {
-                    sunBurn = 1;
-                    StartCoroutine(sunDamage());
-                }
-                else if (timeInSun >= 5 && timeInSun < 15 && canBurn == true)
-                {
-                    sunBurn = 3;
-                    StartCoroutine(sunDamage());
-                }
-                else if (timeInSun >= 15 && timeInSun < 30 && canBurn == true)
-                {
-                    sunBurn = 5;
-                    StartCoroutine(sunDamage());
-                }               
-            }                
-        }                              
+        nightTime -= Time.deltaTime;
     }
 
-    private IEnumerator sunDamage()
+    private void HandleSunExposure()
+    {
+        timeInSun += Time.deltaTime;
+
+        if (IsPlayerInSunlight())
+        {
+            if (timeInSun < 5 && canBurn)
+            {
+                sunBurn = 1;
+                StartCoroutine(SunDamage());
+            }
+            else if (timeInSun >= 5 && timeInSun < 15 && canBurn)
+            {
+                sunBurn = 3;
+                StartCoroutine(SunDamage());
+            }
+            else if (timeInSun >= 15 && timeInSun < 30 && canBurn)
+            {
+                sunBurn = 5;
+                StartCoroutine(SunDamage());
+            }
+        }
+    }
+
+    private bool IsPlayerInSunlight()
+    {
+        return transform.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>());
+    }
+
+    private IEnumerator SunDamage()
     {
         canBurn = false;
-        playerDeath.currentHealth = playerDeath.currentHealth - sunBurn;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().SunRiseDamage(); // Calls The Function "SunRiseDamage" From "playerDeath" Script
-        //GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().showFloatingText(sunBurn);
+        playerDeath.currentHealth -= sunBurn;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().SunRiseDamage();
         yield return new WaitForSeconds(damageDelay);
         canBurn = true;
-    }
-
-    public void ColourChange()
-    {
-        // Update the colourChange value
-        //colourChange += Time.deltaTime / changeSpeed;
-
-        // Get the colour from the gradient
-        //Color currentColor = colorGradient.Evaluate(colourChange);
-
-        // Set the colour of the object
-        //GetComponent<Renderer>().material.color = currentColor;
     }
 }
 
 
-          
+
