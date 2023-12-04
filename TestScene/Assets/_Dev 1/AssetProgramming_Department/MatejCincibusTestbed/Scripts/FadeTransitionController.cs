8d6b72349af42fde60d92edfd8e9f111bcfa7dd3
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-12-04 16:10:48 +0000

    transition fixes

--- TestScene/Assets/_Dev 1/AssetProgramming_Department/MatejCincibusTestbed/Scripts/FadeTransitionController.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MatejCincibusTestbed/Scripts/FadeTransitionController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class FadeTransitionController : MonoBehaviour
 {
     private Animator animator;
-    [SerializeField] private float transitionDuration = 1.5f;
+    [SerializeField] private float transitionDuration = 0f;
 
     private void Start()
     {
@@ -15,15 +15,23 @@ public class FadeTransitionController : MonoBehaviour
 
     public void LoadNextLevel(string levelName)
     {
+
         StartCoroutine(LoadLevel(levelName));
     }
+        
 
     private IEnumerator LoadLevel(string levelName)
     {
+       
+        //Time.timeScale = 1.0f;
         animator.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionDuration);
-
+        if(CanvasManager.Instance != null)
+        {
+            CanvasManager.Instance.deathScreenCanvas.GetComponent<Canvas>().enabled = false;
+        }
+       
         SceneManager.LoadScene(levelName);
     }
 }
--- TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/DeathScreenButtons.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/DeathScreenButtons.cs
@@ -26,23 +26,35 @@ public class DeathScreenButtons : MonoBehaviour
         AudioManager.Manager.PlayMusic("LevelMusic");
         AudioManager.Manager.StopMusic("BossMusic");
 
-        GetComponent<Canvas>().enabled = false;
+        //GetComponent<Canvas>().enabled = false;
+        
+        FindFirstObjectByType<FadeTransitionController>().LoadNextLevel("Spawn");
+
+        Time.timeScale = 1.0f;
+
+
         PlayerController.Instance.GetPlayerDeath().currentHealth = PlayerController.Instance.GetPlayerDeath().maxHealth;
-        PlayerController.Instance.GetPlayerDeath().SetIsDead(false);
+        //PlayerController.Instance.GetPlayerDeath().SetIsDead(false);
         foreach (BoxCollider2D boxCollider in PlayerController.Instance.GetPlayerDeath().boxColliders)
         {
             if (boxCollider.isTrigger)
                 boxCollider.enabled = true;
         }
         PlayerController.Instance.GetPlayerDeath().healthBarScript.SetHealth(PlayerController.Instance.GetPlayerDeath().currentHealth);
+
+        PlayerController.Instance.GetFeeding().currentHunger = 0;
+        CanvasManager.Instance.hungerBarUI.GetComponent<HungerBar>().SetMinHunger(0);
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().setMaxHealth(100);
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().UpdateHealthBarColour(); 
+
         CanvasManager.Instance.countdownTimer.timeRemaining = CanvasManager.Instance.countdownTimer.time;
         Transform rotatingCover = CanvasManager.Instance.countdownTimer.rotatingCover;
         rotatingCover.transform.rotation = Quaternion.Euler(rotatingCover.transform.rotation.x, rotatingCover.transform.rotation.y, 0);
-        deathScreen.enabled = false;
-        FindFirstObjectByType<FadeTransitionController>().LoadNextLevel("Level 1");
+        //deathScreen.enabled = false;
+        
         //SceneManager.LoadScene("Level 1");
-        SetSpawnpoint.instance.ResetPosition();
-        Time.timeScale = 1.0f;
+        //SetSpawnpoint.instance.ResetPosition();
+        
    }
 
     public void Menu()
