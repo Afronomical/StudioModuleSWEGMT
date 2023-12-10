80d5bd53d62f249e35ec944cde0eebd54573598c
TheMGLegends <matejovo34@gmail.com>
2023-12-10 16:58:36 +0000

    Player gets healed after boss fight win

--- TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/WinnerScreenButtons.cs
+++ TestScene/Assets/_Dev 1/AssetProgramming_Department/MattBrakeTestBed/Scripts/WinnerScreenButtons.cs
@@ -10,15 +10,29 @@ public class WinnerScreenButtons : MonoBehaviour
     
     public void EndGame()
     {
+        PlayerController.Instance.GetPlayerDeath().currentHealth = PlayerController.Instance.GetPlayerDeath().maxHealth;
+        PlayerController.Instance.GetPlayerDeath().healthBarScript.SetHealth(PlayerController.Instance.GetPlayerDeath().currentHealth);
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().UpdateHealthBarColour();
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().setMaxHealth(100);
+        PlayerController.Instance.GetFeeding().currentHunger = 0;
+        CanvasManager.Instance.hungerBarUI.GetComponent<HungerBar>().SetMinHunger(0);
+
         //FindFirstObjectByType<FadeTransitionController>().LoadNextLevel("Credits");
         SceneManager.LoadScene("Credits");
         Time.timeScale = 1f;
         AudioManager.Manager.StopMusic("BossMusic");
-        AudioManager.Manager.PlayMusic("Credits"); 
+        AudioManager.Manager.PlayMusic("Credits");
     }
 
     public void Menu()
     {
+        PlayerController.Instance.GetPlayerDeath().currentHealth = PlayerController.Instance.GetPlayerDeath().maxHealth;
+        PlayerController.Instance.GetPlayerDeath().healthBarScript.SetHealth(PlayerController.Instance.GetPlayerDeath().currentHealth);
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().UpdateHealthBarColour();
+        CanvasManager.Instance.HealthBar.GetComponent<NewHealthBarScript>().setMaxHealth(100);
+        PlayerController.Instance.GetFeeding().currentHunger = 0;
+        CanvasManager.Instance.hungerBarUI.GetComponent<HungerBar>().SetMinHunger(0);
+
         AudioManager.Manager.StopMusic("BossMusic");
         //FindFirstObjectByType<FadeTransitionController>().LoadNextLevel("Main Menu Animated");
         SceneManager.LoadScene("Main Menu Animated"); 
